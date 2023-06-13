5dd08a85c8caed3d05251b55d69d3c2bbf7ac48a
Xv1ous <130575612+Xv1ous@users.noreply.github.com>
2023-06-13 21:16:22 +0700

    Add files via upload

---
+++ daafinalproject.py
@@ -0,0 +1,13 @@
+ascii_art = """
+  __        __   _                            _ 
+  \ \      / /__| | ___ ___  _ __ ___   ___  | |
+   \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | |
+    \ V  V /  __/ | (_| (_) | | | | | |  __/ |_|
+     \_/\_/ \___|_|\___\___/|_| |_| |_|\___| (_)
+"""
+
+print(ascii_art)
+
+Username = input("Masukan username anda: ")
+print("Selamat datang,"+Username)
+
