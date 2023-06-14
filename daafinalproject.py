12cefd04d08e5a60bb65dc2010f6c75313405bd5
Bento <74524980+Jody16@users.noreply.github.com>
2023-06-14 14:49:18 +0700

    Update daafinalproject.py 1
    - Penambahan fungsi daftar jadwal
    - Penambahan daftarJadwal dengan dictionary
    - Penambahan daftar jadwal pada menu

--- daafinalproject.py
+++ daafinalproject.py
@@ -1,3 +1,39 @@
+#untuk tampilin Jadwal
+def tampilkan_Jadwal() : 
+    print('Senin : ',daftarJadwal['Senin']['Name'],', ',daftarJadwal['Senin']['Time'])
+    print('Selasa : ',daftarJadwal['Selasa']['Name'],', ',daftarJadwal['Selasa']['Time'])
+    print('Rabu : ',daftarJadwal['Rabu']['Name'],', ',daftarJadwal['Rabu']['Time'])
+    print('Kamis : ',daftarJadwal['Kamis']['Name'],', ',daftarJadwal['Kamis']['Time'])
+    print('Jumat : ',daftarJadwal['Jumat']['Name'],', ',daftarJadwal['Jumat']['Time'])
+
+daftarJadwal = {
+    'Senin' : {
+        'Name' : 'Sena',
+        'Time' : '08:00 - 15:00',
+        'Status' : 'Available'
+    },
+    'Selasa' : {
+        'Name' : 'Selia',
+        'Time' : '08:00 - 15:00',
+        'Status' : 'Available'
+    },
+    'Rabu' : {
+        'Name' : 'Rabita',
+        'Time' : '08:00 - 15:00',
+        'Status' : 'Available'
+    },
+    'Kamis' : {
+        'Name' : 'Kamila',
+        'Time' : '08:00 - 15:00',
+        'Status' : 'Available'
+    },
+    'Jumat' : {
+        'Name' : 'Jumarni',
+        'Time' : '08:00 - 12:00',
+        'Status' : 'Available'
+    },
+}
+
 ascii_art = """
   __        __   _                            _ 
   \ \      / /__| | ___ ___  _ __ ___   ___  | |
@@ -10,4 +46,5 @@ print(ascii_art)
 
 Username = input("Masukan username anda: ")
 print("Selamat datang,"+Username)
+tampilkan_Jadwal()
 
