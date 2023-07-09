3f28034ebf296d13095a4009c71e668032764f44
Xv1ous <130575612+Xv1ous@users.noreply.github.com>
2023-07-09 18:30:45 +0700

    Update tes.py

--- DAA Project/tes.py
+++ DAA Project/tes.py
@@ -1,61 +1,75 @@
 from Jadwal import Bookinglist
 from Jadwal import JadwalKonsuler
-jadwal = [
-    JadwalKonsuler('senin','Sena',True,'08:00 - 12:00'),
-    JadwalKonsuler('senin','Adiarja',True,'12:00 - 16:00'),
-    JadwalKonsuler('selasa','Selia',True,'08:00 - 12:00'),
-    JadwalKonsuler('selasa','Elizabeth',True,'12:00 - 16:00'),
-    JadwalKonsuler('rabu','Rabita',True,'08:00 - 12:00'),
-    JadwalKonsuler('rabu','Leon',True,'12:00 - 16:00'),
-    JadwalKonsuler('kamis','Kamila',True,'08:00 - 12:00'),
-    JadwalKonsuler('kamis','Tiffa',True,'12:00 - 16:00'),
-    JadwalKonsuler('jumat','Jumarni',True,'08:00 - 12:00'),
-    JadwalKonsuler('jumat','Kafka',True,'12:00 - 16:00')
-]
 
-n=0
+jadwal = {
+    'senin_a': JadwalKonsuler('senin', 'Sena', True, '08:00 - 12:00'),
+    'senin_b': JadwalKonsuler('senin', 'Adiarja', True, '12:00 - 16:00'),
+    'selasa_a': JadwalKonsuler('selasa', 'Selia', True, '08:00 - 12:00'),
+    'selasa_b': JadwalKonsuler('selasa', 'Elizabeth', True, '12:00 - 16:00'),
+    'rabu_a': JadwalKonsuler('rabu', 'Rabita', True, '08:00 - 12:00'),
+    'rabu_b': JadwalKonsuler('rabu', 'Leon', True, '12:00 - 16:00'),
+    'kamis_a': JadwalKonsuler('kamis', 'Kamila', True, '08:00 - 12:00'),
+    'kamis_b': JadwalKonsuler('kamis', 'Tiffa', True, '12:00 - 16:00'),
+    'jumat_a': JadwalKonsuler('jumat', 'Jumarni', True, '08:00 - 12:00'),
+    'jumat_b': JadwalKonsuler('jumat', 'Kafka', True, '12:00 - 16:00')
+}
 
-hari_konseling = input('Masukkan hari yang akan di pilih:').lower()
-jam_konseling = input('Masukan jam yang ingin pilih (08:00 - 12:00(A) dan 12:00 - 16:00(B)): ').lower()
-if hari_konseling == 'senin' and jam_konseling == 'a':
-    n = 0
-elif hari_konseling == 'senin' and jam_konseling == 'b':
-    n = 1
-elif hari_konseling == 'selasa' and jam_konseling == 'a':
-    n = 2
-elif hari_konseling == 'selasa' and jam_konseling == 'b':
-    n = 3
-elif hari_konseling == 'rabu' and jam_konseling == 'a':
-    n = 4
-elif hari_konseling == 'rabu' and jam_konseling == 'b':
-    n = 5
-elif hari_konseling == 'kamis' and jam_konseling == 'a':
-    n = 6
-elif hari_konseling == 'kamis' and jam_konseling == 'b':
-    n = 7
-elif hari_konseling == 'jumat' and jam_konseling == 'a':
-    n = 8
-elif hari_konseling == 'jumat' and jam_konseling == 'b':
-    n = 9
-print("Masukan biodata anda:")
-nama = input("Nama : ")
-nim = input("NIM : ")
-hp = input("No HP : ")
-umur = input("Umur : ")
+nama_file = 'data_booking.txt'  # Nama file untuk menyimpan data
 
-jadwal[n].jadwalKonsuler_status = False
-print('Jadwal telah di tambahkan \n')
+# Fungsi untuk menyimpan data ke dalam file
+def simpan_data_ke_file(data, nama_file):
+    with open(nama_file, 'w') as file:
+        for key, value in data.items():
+            if key in jadwal:  # Memastikan hanya menyimpan data yang ada di jadwal
+                file.write(f"{key},{value}\n")
 
-booking = Bookinglist(nama, nim, hp, umur)
+# Fungsi untuk membaca data dari file
+def baca_data_dari_file(nama_file):
+    data = {}
+    with open(nama_file, 'r') as file:
+        for line in file:
+            key, value = line.strip().split(',')
+            data[key] = value
+    return data
 
-for jadwal_konsuler in jadwal:
-    print('================================================================')
-    print(f"Hari: {jadwal_konsuler.jadwalKonsuler_hari}")
-    print(f"Nama: {jadwal_konsuler.jadwalKonsuler_nama}")
-    print(f"Status: {'Tersedia' if jadwal_konsuler.jadwalKonsuler_status else 'Terisi'}")
-    print(f"Jam: {jadwal_konsuler.jadwalKonsuler_waktu}")
-    print(f"Nama: {booking.bookinglist_name}")
-    print(f"NIM: {booking.bookinglist_nim}")
-    print(f"No HP: {booking.bookinglist_hp}")
-    print(f"Umur: {booking.bookinglist_umur}")
-    print('================================================================')
+# Mengambil data booking dari file (jika ada)
+try:
+    data_booking = baca_data_dari_file(nama_file)
+except FileNotFoundError:
+    data_booking = {}
+
+# Masukkan biodata jika jadwal tersedia
+hari_konseling = input('Masukkan hari yang akan dipilih:').lower()
+jam_konseling = input('Masukkan jam yang ingin dipilih (08:00 - 12:00(A) dan 12:00 - 16:00(B)): ').lower()
+key = f"{hari_konseling}_{jam_konseling}"
+if key in jadwal and jadwal[key].jadwalKonsuler_status:
+    print("Masukkan biodata anda:")
+    nama = input("Nama : ")
+    nim = input("NIM : ")
+    hp = input("No HP : ")
+    umur = input("Umur : ")
+
+    jadwal[key].jadwalKonsuler_status = False
+    print('Jadwal telah ditambahkan \n')
+
+    data_booking[key] = nama
+else:
+    print('Jadwal sudah penuh \n')
+
+# Simpan data booking ke file
+simpan_data_ke_file(data_booking, nama_file)
+
+# Cetak jadwal dan data booking
+for key, value in jadwal.items():
+    print('=====================================')
+    print('Hari : ', value.jadwalKonsuler_hari)
+    print('Konsuler : ', value.jadwalKonsuler_nama)
+    print('Waktu : ', value.jadwalKonsuler_waktu)
+
+    if key in data_booking:
+        print('Status : Not Available')
+        print('di Booking oleh : ', data_booking[key])
+    else:
+        print('Status : Available')
+    
+    print('=====================================')
